cf254a89acc3d3e2b83e097c7a3c2b2810c9ac2e
Edmundo <jmundo94@gmail.com>
2014-11-25 12:37:37 -0600

    CardView created

--- app/src/main/java/com/jele/brasilapp/MainActivity.java
+++ app/src/main/java/com/jele/brasilapp/MainActivity.java
@@ -2,17 +2,24 @@ package com.jele.brasilapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends Activity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
 
 
     @Override
